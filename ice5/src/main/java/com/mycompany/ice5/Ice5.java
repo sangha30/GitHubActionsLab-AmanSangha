59cf6bbd5d4c7cb217b81dd9103e5ac34df6472b
sangh <sangh@AMANPC>
2026-02-11 14:05:45 -0500

    Initial Commit

---
+++ ice5/src/main/java/com/mycompany/ice5/Ice5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice5;
+
+/**
+ *
+ * @author sangh
+ */
+public class Ice5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
